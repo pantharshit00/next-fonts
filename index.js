4d790d97bde9e146f7b31dfaf6bc2cffc804cc2c
Harshit Pant <pantharshit00@gmail.com>
2018-09-29 23:32:31 +0530

    feat: add asset path

--- index.js
+++ index.js
@@ -4,30 +4,32 @@ module.exports = (nextConfig = {}) => {
       if (!options.defaultLoaders) {
         throw new Error(
           'This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade'
-        )
+        );
       }
+
+      const assetPrefix = nextConfig.assetPrefix || '';
 
       config.module.rules.push({
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
           {
-            loader: "url-loader",
+            loader: 'url-loader',
             options: {
               limit: 8192,
-              fallback: "file-loader",
-              publicPath: "/_next/static/fonts/",
-              outputPath: "static/fonts/",
-              name: "[name]-[hash].[ext]"
+              fallback: 'file-loader',
+              publicPath: `${assetPrefix}/_next/static/fonts/`,
+              outputPath: 'static/fonts/',
+              name: '[name]-[hash].[ext]'
             }
           }
         ]
       });
 
       if (typeof nextConfig.webpack === 'function') {
-        return nextConfig.webpack(config, options)
+        return nextConfig.webpack(config, options);
       }
 
-      return config
+      return config;
     }
-  })
-}
+  });
+};
